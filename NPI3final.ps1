3074df14f9c5f7123bfdabde8336a0021fe1c76d
daojah <bushmakinandrey@gmail.com>
2015-10-30 11:05:19 +0200

    Update NPI3final.ps1

--- NPI3final.ps1
+++ NPI3final.ps1
@@ -1,5 +1,5 @@
-﻿$user = "username"
-$pwd = "password"
+﻿$user = ""
+$pwd = ""
 $de = "LDAP://[SERVERNAME]/cn=user,ou=people,o=company"
 $deObject = New-Object -TypeName System.DirectoryServices.DirectoryEntry($de,$user,$pwd,'FastBind')
 $selector = new-Object System.DirectoryServices.DirectorySearcher($deObject)
